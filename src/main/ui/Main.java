d1bf6e3bc2efe8a46e36903031b3b5d89413689a
rchanpra <rchanpra@students.cs.ubc.ca>
2023-03-01 17:50:04 -0800

    1.2.1 - Added documentation or Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,8 +2,10 @@ package ui;
 
 import java.util.Scanner;
 
+// Represents a main class to declare and initiate game
 public class Main {
 
+    // EFFECTS: declare and initiate a game object with given input, then call run on game object
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your initial balance: ");
